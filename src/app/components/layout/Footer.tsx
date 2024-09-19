0749058cb5c41afe29f7a08b79a54a8148dd5428
Javier Rugel <jrugel@gmail.com>
2024-09-19 11:56:02 -0400

    Cosmetic on footer

--- src/app/components/layout/Footer.tsx
+++ src/app/components/layout/Footer.tsx
@@ -2,5 +2,9 @@ import React from "react";
 import { DEFAULT_MAIN_BG } from "@/lib/constants";
 
 export default function Footer() {
-  return <footer className={DEFAULT_MAIN_BG + " text-center"}>Footer</footer>;
+  return (
+    <footer className={DEFAULT_MAIN_BG}>
+      <div className="container mx-auto text-center">Footer</div>
+    </footer>
+  );
 }
